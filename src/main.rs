703f26a2a58b086c25d3cf91136b720b18f035d9
matej-kotrba <matejkotrba.skola@gmail.com>
2024-07-19 00:40:03 +0200

    Add game board initialization and rendering logic

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,64 @@
 use raylib::prelude::*;
 
 struct Game {
-    board: Board,
+    tiles: [[Tile; Self::SIZE]; Self::SIZE],
 }
 
 impl Game {
-    pub fn new() -> Self {
-        let mut tiles = [[Tile::new(); Board::SIZE]; Board::SIZE];
+    pub const SIZE: usize = 8;
 
-        for y in 0..Board::SIZE {
-            for x in 0..Board::SIZE {
+    pub fn new() -> Self {
+        let mut tiles = [[Tile::new(); Self::SIZE]; Self::SIZE];
+
+        for y in 0..Self::SIZE {
+            for x in 0..Self::SIZE {
                 if (x + y % 2) % 2 == 0 {
                     tiles[y][x].color = Color::WHITE;
                 }
             }
         }
 
-        Game {
-            board: Board { tiles: tiles },
-        }
+        let mut game = Game { tiles };
+        game.reset();
+
+        game
     }
     pub fn render(&self, d: &mut RaylibDrawHandle) {
-        let size = WINDOW_WIDTH as usize / Board::SIZE;
-        for y in 0..Board::SIZE {
-            for x in 0..Board::SIZE {
+        let size = WINDOW_WIDTH as usize / Self::SIZE;
+        for y in 0..Self::SIZE {
+            for x in 0..Self::SIZE {
                 d.draw_rectangle(
                     (x * size) as i32,
                     (y * size) as i32,
                     size as i32,
                     size as i32,
-                    self.board.tiles[y][x].color,
+                    self.tiles[y][x].color,
                 );
             }
         }
     }
-}
+    pub fn reset(&mut self) {
+        use PieceType::*;
+        let backrow: [PieceType; Self::SIZE] =
+            [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook];
+        let frontrow: [PieceType; Self::SIZE] = [Pawn; Self::SIZE];
 
-struct Board {
-    tiles: [[Tile; Self::SIZE]; Self::SIZE],
-}
+        for (index, piece) in backrow.iter().enumerate() {
+            self.tiles[0][index].piece = Some(Piece::new(*piece, Side::Black));
+        }
 
-impl Board {
-    pub const SIZE: usize = 8;
+        for (index, piece) in frontrow.iter().enumerate() {
+            self.tiles[1][index].piece = Some(Piece::new(*piece, Side::Black));
+        }
+
+        for (index, piece) in frontrow.iter().enumerate() {
+            self.tiles[Self::SIZE - 2][index].piece = Some(Piece::new(*piece, Side::White));
+        }
+
+        for (index, piece) in backrow.iter().enumerate() {
+            self.tiles[Self::SIZE - 1][index].piece = Some(Piece::new(*piece, Side::White));
+        }
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -57,10 +74,72 @@ impl Tile {
             piece: None,
         }
     }
+
+    pub fn render(&self, d: &mut RaylibDrawHandle, (x, y): (i32, i32)) {
+        match self.piece {
+            Some(piece) => d.draw_text(&piece.to_string(), x, y, 18, Color::PURPLE),
+            None => {}
+        }
+    }
 }
 
 #[derive(Copy, Clone)]
-struct Piece {}
+struct Piece {
+    kind: PieceType,
+    side: Side,
+    did_move: bool,
+}
+
+impl Piece {
+    pub fn new(kind: PieceType, side: Side) -> Self {
+        Piece {
+            kind,
+            side,
+            did_move: false,
+        }
+    }
+}
+
+impl std::fmt::Display for Piece {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.kind {
+            PieceType::Pawn => {
+                write!(f, "Pawn")
+            }
+            PieceType::Rook => {
+                write!(f, "Rook")
+            }
+            PieceType::Knight => {
+                write!(f, "Knight")
+            }
+            PieceType::Bishop => {
+                write!(f, "Bishop")
+            }
+            PieceType::Queen => {
+                write!(f, "Queen")
+            }
+            PieceType::King => {
+                write!(f, "King")
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum PieceType {
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King,
+}
+
+#[derive(Copy, Clone)]
+enum Side {
+    Black,
+    White,
+}
 
 const WINDOW_WIDTH: i32 = 800;
 const WINDOW_HEIGHT: i32 = 800;
@@ -77,5 +156,17 @@ fn main() {
         let mut d = rl.begin_drawing(&thread);
 
         game.render(&mut d);
+
+        for y in 0..Game::SIZE {
+            for x in 0..Game::SIZE {
+                game.tiles[y][x].render(
+                    &mut d,
+                    (
+                        x as i32 * (WINDOW_WIDTH / Game::SIZE as i32) + 10,
+                        y as i32 * (WINDOW_HEIGHT / Game::SIZE as i32) + 10,
+                    ),
+                )
+            }
+        }
     }
 }
